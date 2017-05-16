5d364646bb56f15b9a75ca6ac8431583b1179122
wemoloh <bart@bartrobinson.com>
2017-05-16 12:02:59 -0600

    async cxt mgr was removed from RemoteSession

--- simple_client_async.py
+++ simple_client_async.py
@@ -5,7 +5,7 @@ obj = {'looks': how, 'swims': how, 'quacks': how}
 
 async def ducktest():
     async with eider.Connection('ws://localhost:12345') as conn:
-        async with conn.create_session() as duck_tester:
+        with conn.create_session() as duck_tester:
             is_duck = await duck_tester.is_it_a_duck(obj)
     
     print("It's probably " + ("" if is_duck else "NOT ") + "a duck.")
