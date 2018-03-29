9cd172bed4444248c515c6b10174ae629d8a2324
wemoloh <bart@bartrobinson.com>
2018-03-28 18:37:56 -0600

    bumped version to 0.11.0

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ copyright = '%d, ON Semiconductor' % datetime.date.today().year
 author = 'Bart Robinson'
 
 # The short X.Y version
-version = '0.10.0'
+version = '0.11.0'
 # The full version, including alpha/beta/rc tags
 release = version
 
